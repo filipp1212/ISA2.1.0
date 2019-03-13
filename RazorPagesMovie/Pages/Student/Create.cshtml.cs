f4461ce7ccb06401b10a9455ada960a8be03a568
Filipp Djatšuk <fidjat@ttu.ee>
2019-03-13 11:31:04 +0200

    kd

---
+++ RazorPagesMovie/Pages/Student/Create.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using RazorPageMovies.Models;
+using RazorPagesMovie.Models;
+
+namespace RazorPageMovies.Pages.Student
+{
+    public class CreateModel : PageModel
+    {
+        private readonly RazorPageMovies.Models.Context _context;
+
+        public CreateModel(RazorPageMovies.Models.Context context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public RazorPagesMovie.Models.Student Student { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Student.Add(Student);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ RazorPagesMovie/Pages/Student/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RazorPageMovies.Models;
+using RazorPagesMovie.Models;
+
+namespace RazorPageMovies.Pages.Student
+{
+    public class IndexModel : PageModel
+    {
+        private readonly RazorPageMovies.Models.Context _context;
+
+        public IndexModel(RazorPageMovies.Models.Context context)
+        {
+            _context = context;
+        }
+
+        public IList<RazorPagesMovie.Models.Student> Student { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Student = await _context.Student.ToListAsync();
+        }
+    }
+}
