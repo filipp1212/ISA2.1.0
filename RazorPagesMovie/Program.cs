eb655e9d2224414378df2bc47e0bf56ef14d439b
Filipp Djatšuk <fidjat@ttu.ee>
2019-03-16 14:13:57 +0200

    K8

--- RazorPagesMovie/Program.cs
+++ RazorPagesMovie/Program.cs
@@ -3,21 +3,39 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using RazorPageMovie.Models;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.DependencyInjection;   // CreateScope
-using RazorPageMovies.Models;
+using RazorPagesMovie;
 
-
-namespace RazorPagesMovie
+namespace RazorPagesMovies
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    var context = services.GetRequiredService<SchoolContext>();
+                    // using ContosoUniversity.Data; 
+                    DbInitializer.Initialize(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred creating the DB.");
+                }
+            }
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
